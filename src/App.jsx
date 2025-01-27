a3f661b09b76e614717f22b84b61e02e54d46c19
manuelloza_bo <manuel.loza.c@gmail.com>
2025-01-27 01:33:21 -0400

    Final Update

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Navigator from './Components/Navigation/Navigator.jsx'
 import Router from './Components/Navigation/Router.jsx'
 function App() {
   return (
-    <>
+    <> 
       <Navigator/>
       <Router/>
     </>
